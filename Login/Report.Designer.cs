a1119700a5b46cbf7f01ad21f452d55b1b48e6da
allainrico <allainrico@gmail.com>
2023-03-19 14:42:09 +0800

    finished datagridview for reports

--- Login/Report.Designer.cs
+++ Login/Report.Designer.cs
@@ -33,7 +33,17 @@
             this.goAdminHome = new System.Windows.Forms.ToolStripMenuItem();
             this.goBooks = new System.Windows.Forms.ToolStripMenuItem();
             this.goBorrower = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.displayallbutton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.tolabel = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.fromlabel = new System.Windows.Forms.Label();
+            this.datefilterbutton = new System.Windows.Forms.Button();
             this.menuStrip2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip2
@@ -53,8 +63,8 @@
             this.goBooks,
             this.goBorrower});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.fileToolStripMenuItem.Text = "Navigate";
             // 
             // goAdminHome
             // 
@@ -77,17 +87,105 @@
             this.goBorrower.Text = "Borrower";
             this.goBorrower.Click += new System.EventHandler(this.goBorrower_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.Location = new System.Drawing.Point(10, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(597, 421);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // displayallbutton
+            // 
+            this.displayallbutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.displayallbutton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.displayallbutton.Location = new System.Drawing.Point(613, 27);
+            this.displayallbutton.Name = "displayallbutton";
+            this.displayallbutton.Size = new System.Drawing.Size(181, 31);
+            this.displayallbutton.TabIndex = 19;
+            this.displayallbutton.Text = "Display All";
+            this.displayallbutton.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.datefilterbutton);
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Controls.Add(this.tolabel);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.fromlabel);
+            this.groupBox1.Location = new System.Drawing.Point(613, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(181, 127);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker2.Location = new System.Drawing.Point(53, 56);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(114, 20);
+            this.dateTimePicker2.TabIndex = 21;
+            // 
+            // tolabel
+            // 
+            this.tolabel.AutoSize = true;
+            this.tolabel.Location = new System.Drawing.Point(13, 56);
+            this.tolabel.Name = "tolabel";
+            this.tolabel.Size = new System.Drawing.Size(23, 13);
+            this.tolabel.TabIndex = 20;
+            this.tolabel.Text = "To:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(53, 24);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(114, 20);
+            this.dateTimePicker1.TabIndex = 19;
+            // 
+            // fromlabel
+            // 
+            this.fromlabel.AutoSize = true;
+            this.fromlabel.Location = new System.Drawing.Point(13, 27);
+            this.fromlabel.Name = "fromlabel";
+            this.fromlabel.Size = new System.Drawing.Size(33, 13);
+            this.fromlabel.TabIndex = 18;
+            this.fromlabel.Text = "From:";
+            // 
+            // datefilterbutton
+            // 
+            this.datefilterbutton.Location = new System.Drawing.Point(104, 83);
+            this.datefilterbutton.Name = "datefilterbutton";
+            this.datefilterbutton.Size = new System.Drawing.Size(63, 26);
+            this.datefilterbutton.TabIndex = 22;
+            this.datefilterbutton.Text = "Apply";
+            this.datefilterbutton.UseVisualStyleBackColor = true;
+            // 
             // Report
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.displayallbutton);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.menuStrip2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Report";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Report";
+            this.Load += new System.EventHandler(this.Report_Load);
             this.menuStrip2.ResumeLayout(false);
             this.menuStrip2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,5 +198,13 @@
         private System.Windows.Forms.ToolStripMenuItem goAdminHome;
         private System.Windows.Forms.ToolStripMenuItem goBooks;
         private System.Windows.Forms.ToolStripMenuItem goBorrower;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button displayallbutton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label tolabel;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label fromlabel;
+        private System.Windows.Forms.Button datefilterbutton;
     }
 }
--- Login/Report.cs
+++ Login/Report.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -37,6 +38,19 @@ namespace Login
             borrower borrower = new borrower();
             borrower.Show();
             Visible = false;
+        }
+
+        private void Report_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ("Data Source=DESKTOP-SKI34QJ\\SQLEXPRESS;Initial Catalog=libsysdb;Integrated Security=True");
+            con.Open();
+
+            SqlDataAdapter sqlData = new SqlDataAdapter("select borrowTable.Status, borrowTable.username, borrowTable.bookID, borrowTable.borrowDate, returnTable.returnDate from borrowTable INNER join returnTable on borrowTable.bookID = returnTable.bookID", con);
+            DataTable dtbl = new DataTable();
+            sqlData.Fill(dtbl);
+
+            dataGridView1.DataSource = dtbl;
         }
     }
 }
