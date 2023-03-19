ed979219db33d3266d965323a0602462102ab5e8
allainrico <allainrico@gmail.com>
2023-03-19 22:54:34 +0800

    UPDATED design for sign up form

--- Login/signUp.Designer.cs
+++ Login/signUp.Designer.cs
@@ -37,7 +37,9 @@
             this.passwordLabel = new System.Windows.Forms.Label();
             this.usernamelabel = new System.Windows.Forms.Label();
             this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -51,39 +53,47 @@
             this.groupBox1.Controls.Add(this.usernamelabel);
             this.groupBox1.Controls.Add(this.usernameTextBox);
             this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.groupBox1.Location = new System.Drawing.Point(24, 42);
+            this.groupBox1.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(35)))), ((int)(((byte)(43)))));
+            this.groupBox1.Location = new System.Drawing.Point(417, 31);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(252, 222);
+            this.groupBox1.Size = new System.Drawing.Size(354, 481);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // confirmPasswordTextBox
             // 
-            this.confirmPasswordTextBox.Location = new System.Drawing.Point(116, 101);
+            this.confirmPasswordTextBox.BackColor = System.Drawing.Color.White;
+            this.confirmPasswordTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(35)))), ((int)(((byte)(43)))));
+            this.confirmPasswordTextBox.Location = new System.Drawing.Point(107, 220);
             this.confirmPasswordTextBox.Name = "confirmPasswordTextBox";
-            this.confirmPasswordTextBox.Size = new System.Drawing.Size(126, 20);
+            this.confirmPasswordTextBox.Size = new System.Drawing.Size(226, 26);
             this.confirmPasswordTextBox.TabIndex = 7;
             this.confirmPasswordTextBox.UseSystemPasswordChar = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 101);
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(35)))), ((int)(((byte)(43)))));
+            this.label1.Location = new System.Drawing.Point(16, 224);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.Size = new System.Drawing.Size(67, 16);
             this.label1.TabIndex = 6;
-            this.label1.Text = "Confirm Password";
+            this.label1.Text = "Confirm";
             // 
             // signinButton
             // 
-            this.signinButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.signinButton.BackColor = System.Drawing.Color.White;
+            this.signinButton.FlatAppearance.BorderSize = 0;
             this.signinButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.signinButton.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.signinButton.Location = new System.Drawing.Point(16, 171);
+            this.signinButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(101)))), ((int)(((byte)(107)))));
+            this.signinButton.Location = new System.Drawing.Point(19, 309);
             this.signinButton.Margin = new System.Windows.Forms.Padding(0);
             this.signinButton.Name = "signinButton";
-            this.signinButton.Size = new System.Drawing.Size(225, 23);
+            this.signinButton.Size = new System.Drawing.Size(315, 37);
             this.signinButton.TabIndex = 5;
             this.signinButton.Text = "Already have an account?";
             this.signinButton.UseVisualStyleBackColor = false;
@@ -91,52 +101,78 @@
             // 
             // signupbutton
             // 
-            this.signupbutton.Location = new System.Drawing.Point(16, 138);
+            this.signupbutton.BackColor = System.Drawing.Color.White;
+            this.signupbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.signupbutton.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Bold);
+            this.signupbutton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(35)))), ((int)(((byte)(43)))));
+            this.signupbutton.Location = new System.Drawing.Point(19, 270);
             this.signupbutton.Name = "signupbutton";
-            this.signupbutton.Size = new System.Drawing.Size(225, 26);
+            this.signupbutton.Size = new System.Drawing.Size(315, 37);
             this.signupbutton.TabIndex = 4;
             this.signupbutton.Text = "Register";
-            this.signupbutton.UseVisualStyleBackColor = true;
+            this.signupbutton.UseVisualStyleBackColor = false;
             this.signupbutton.Click += new System.EventHandler(this.signupbutton_Click);
             // 
             // passwordTextBox
             // 
-            this.passwordTextBox.Location = new System.Drawing.Point(115, 69);
+            this.passwordTextBox.BackColor = System.Drawing.Color.White;
+            this.passwordTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(35)))), ((int)(((byte)(43)))));
+            this.passwordTextBox.Location = new System.Drawing.Point(107, 184);
             this.passwordTextBox.Name = "passwordTextBox";
-            this.passwordTextBox.Size = new System.Drawing.Size(126, 20);
+            this.passwordTextBox.Size = new System.Drawing.Size(226, 26);
             this.passwordTextBox.TabIndex = 3;
             this.passwordTextBox.UseSystemPasswordChar = true;
             // 
             // passwordLabel
             // 
             this.passwordLabel.AutoSize = true;
-            this.passwordLabel.Location = new System.Drawing.Point(13, 69);
+            this.passwordLabel.BackColor = System.Drawing.Color.White;
+            this.passwordLabel.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Bold);
+            this.passwordLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(35)))), ((int)(((byte)(43)))));
+            this.passwordLabel.Location = new System.Drawing.Point(15, 188);
             this.passwordLabel.Name = "passwordLabel";
-            this.passwordLabel.Size = new System.Drawing.Size(53, 13);
+            this.passwordLabel.Size = new System.Drawing.Size(76, 16);
             this.passwordLabel.TabIndex = 2;
             this.passwordLabel.Text = "Password";
             // 
             // usernamelabel
             // 
             this.usernamelabel.AutoSize = true;
-            this.usernamelabel.Location = new System.Drawing.Point(12, 35);
+            this.usernamelabel.BackColor = System.Drawing.Color.White;
+            this.usernamelabel.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Bold);
+            this.usernamelabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(35)))), ((int)(((byte)(43)))));
+            this.usernamelabel.Location = new System.Drawing.Point(14, 150);
             this.usernamelabel.Name = "usernamelabel";
-            this.usernamelabel.Size = new System.Drawing.Size(55, 13);
+            this.usernamelabel.Size = new System.Drawing.Size(79, 16);
             this.usernamelabel.TabIndex = 0;
             this.usernamelabel.Text = "Username";
             // 
             // usernameTextBox
             // 
-            this.usernameTextBox.Location = new System.Drawing.Point(115, 32);
+            this.usernameTextBox.BackColor = System.Drawing.Color.White;
+            this.usernameTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(35)))), ((int)(((byte)(43)))));
+            this.usernameTextBox.Location = new System.Drawing.Point(107, 147);
             this.usernameTextBox.Name = "usernameTextBox";
-            this.usernameTextBox.Size = new System.Drawing.Size(126, 20);
+            this.usernameTextBox.Size = new System.Drawing.Size(226, 26);
             this.usernameTextBox.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Login.Properties.Resources.books;
+            this.pictureBox1.Location = new System.Drawing.Point(62, 146);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(265, 234);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
             // 
             // signUp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(301, 305);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.groupBox1);
             this.Name = "signUp";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -144,6 +180,7 @@
             this.Load += new System.EventHandler(this.signUp_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -159,5 +196,6 @@
         private System.Windows.Forms.TextBox usernameTextBox;
         private System.Windows.Forms.TextBox confirmPasswordTextBox;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Login/userHome.Designer.cs
+++ Login/userHome.Designer.cs
@@ -28,12 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(userHome));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.transactionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.borrowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.returnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.gosignout = new System.Windows.Forms.ToolStripMenuItem();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.updatebutton = new System.Windows.Forms.Button();
             this.booksreturnedtext = new System.Windows.Forms.TextBox();
             this.booksborrowedtext = new System.Windows.Forms.TextBox();
@@ -51,15 +54,13 @@
             this.borrowerIDlabel = new System.Windows.Forms.Label();
             this.borrowbutton = new System.Windows.Forms.Button();
             this.returnbookbutton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.menuStrip1.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -127,6 +128,29 @@
             this.groupBox1.Size = new System.Drawing.Size(422, 484);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Login.Properties.Resources.user_creds_design;
+            this.pictureBox3.Location = new System.Drawing.Point(30, 17);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(94, 88);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 18;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(145, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "User Personal Information";
             // 
             // updatebutton
             // 
@@ -359,19 +383,6 @@
             this.returnbookbutton.UseVisualStyleBackColor = false;
             this.returnbookbutton.Click += new System.EventHandler(this.returnbookbutton_Click);
             // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(145, 53);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(198, 16);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "User Personal Information";
-            // 
             // pictureBox2
             // 
             this.pictureBox2.Image = global::Login.Properties.Resources.return_books;
@@ -392,16 +403,6 @@
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = global::Login.Properties.Resources.user_creds_design;
-            this.pictureBox3.Location = new System.Drawing.Point(30, 17);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(94, 88);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 18;
-            this.pictureBox3.TabStop = false;
-            // 
             // userHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -414,6 +415,7 @@
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.borrowbutton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "userHome";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.userHome_Load);
@@ -421,9 +423,9 @@
             this.menuStrip1.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
