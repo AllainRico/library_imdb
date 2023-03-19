48b9049efa0dafabed4a6d175e06d7d109fd42da
allainrico <allainrico@gmail.com>
2023-03-19 23:53:33 +0800

    DESIGN UPDATE on ui for borrowers form

--- Login/borrower.Designer.cs
+++ Login/borrower.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(borrower));
             this.borrowergridview = new System.Windows.Forms.DataGridView();
             this.displayallborrowerbutton = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
@@ -53,10 +54,12 @@
             this.borrowerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.booksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.reportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.borrowergridview)).BeginInit();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.menuStrip2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // borrowergridview
@@ -64,18 +67,22 @@
             this.borrowergridview.BackgroundColor = System.Drawing.Color.White;
             this.borrowergridview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.borrowergridview.GridColor = System.Drawing.Color.White;
-            this.borrowergridview.Location = new System.Drawing.Point(12, 30);
+            this.borrowergridview.Location = new System.Drawing.Point(12, 27);
             this.borrowergridview.Name = "borrowergridview";
-            this.borrowergridview.Size = new System.Drawing.Size(552, 418);
+            this.borrowergridview.Size = new System.Drawing.Size(638, 500);
             this.borrowergridview.TabIndex = 0;
             // 
             // displayallborrowerbutton
             // 
-            this.displayallborrowerbutton.Location = new System.Drawing.Point(570, 34);
+            this.displayallborrowerbutton.FlatAppearance.BorderSize = 0;
+            this.displayallborrowerbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.displayallborrowerbutton.Font = new System.Drawing.Font("Mongolian Baiti", 11.25F, System.Drawing.FontStyle.Bold);
+            this.displayallborrowerbutton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(35)))), ((int)(((byte)(43)))));
+            this.displayallborrowerbutton.Location = new System.Drawing.Point(737, 39);
             this.displayallborrowerbutton.Name = "displayallborrowerbutton";
-            this.displayallborrowerbutton.Size = new System.Drawing.Size(223, 31);
+            this.displayallborrowerbutton.Size = new System.Drawing.Size(181, 31);
             this.displayallborrowerbutton.TabIndex = 1;
-            this.displayallborrowerbutton.Text = "Display all Borrower";
+            this.displayallborrowerbutton.Text = "Display All Borrowers";
             this.displayallborrowerbutton.UseVisualStyleBackColor = true;
             this.displayallborrowerbutton.Click += new System.EventHandler(this.displayallborrowerbutton_Click);
             // 
@@ -90,25 +97,29 @@
             this.groupBox2.Controls.Add(this.addborrowerfirstnametext);
             this.groupBox2.Controls.Add(this.userfirstnamelabel);
             this.groupBox2.Controls.Add(this.userID);
-            this.groupBox2.Location = new System.Drawing.Point(570, 228);
+            this.groupBox2.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F);
+            this.groupBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(35)))), ((int)(((byte)(43)))));
+            this.groupBox2.Location = new System.Drawing.Point(656, 258);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(223, 179);
+            this.groupBox2.Size = new System.Drawing.Size(253, 207);
             this.groupBox2.TabIndex = 5;
             this.groupBox2.TabStop = false;
             // 
             // usernamecombobox
             // 
             this.usernamecombobox.FormattingEnabled = true;
-            this.usernamecombobox.Location = new System.Drawing.Point(100, 19);
+            this.usernamecombobox.Location = new System.Drawing.Point(106, 33);
             this.usernamecombobox.Name = "usernamecombobox";
-            this.usernamecombobox.Size = new System.Drawing.Size(118, 21);
+            this.usernamecombobox.Size = new System.Drawing.Size(129, 22);
             this.usernamecombobox.TabIndex = 12;
             // 
             // addborrowerbutton
             // 
-            this.addborrowerbutton.Location = new System.Drawing.Point(147, 141);
+            this.addborrowerbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addborrowerbutton.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F);
+            this.addborrowerbutton.Location = new System.Drawing.Point(147, 157);
             this.addborrowerbutton.Name = "addborrowerbutton";
-            this.addborrowerbutton.Size = new System.Drawing.Size(70, 23);
+            this.addborrowerbutton.Size = new System.Drawing.Size(87, 24);
             this.addborrowerbutton.TabIndex = 12;
             this.addborrowerbutton.Text = "Add";
             this.addborrowerbutton.UseVisualStyleBackColor = true;
@@ -116,58 +127,58 @@
             // 
             // addborrowercontactnumbertext
             // 
-            this.addborrowercontactnumbertext.Location = new System.Drawing.Point(100, 115);
+            this.addborrowercontactnumbertext.Location = new System.Drawing.Point(106, 129);
             this.addborrowercontactnumbertext.Name = "addborrowercontactnumbertext";
-            this.addborrowercontactnumbertext.Size = new System.Drawing.Size(117, 20);
+            this.addborrowercontactnumbertext.Size = new System.Drawing.Size(128, 22);
             this.addborrowercontactnumbertext.TabIndex = 7;
             // 
             // contactnumbertext
             // 
             this.contactnumbertext.AutoSize = true;
-            this.contactnumbertext.Location = new System.Drawing.Point(7, 115);
+            this.contactnumbertext.Location = new System.Drawing.Point(7, 129);
             this.contactnumbertext.Name = "contactnumbertext";
-            this.contactnumbertext.Size = new System.Drawing.Size(87, 13);
+            this.contactnumbertext.Size = new System.Drawing.Size(100, 14);
             this.contactnumbertext.TabIndex = 6;
             this.contactnumbertext.Text = "Contact Number:";
             // 
             // addborrowerlastnametext
             // 
-            this.addborrowerlastnametext.Location = new System.Drawing.Point(100, 86);
+            this.addborrowerlastnametext.Location = new System.Drawing.Point(106, 100);
             this.addborrowerlastnametext.Name = "addborrowerlastnametext";
-            this.addborrowerlastnametext.Size = new System.Drawing.Size(117, 20);
+            this.addborrowerlastnametext.Size = new System.Drawing.Size(128, 22);
             this.addborrowerlastnametext.TabIndex = 5;
             // 
             // userlastnamelabel
             // 
             this.userlastnamelabel.AutoSize = true;
-            this.userlastnamelabel.Location = new System.Drawing.Point(7, 89);
+            this.userlastnamelabel.Location = new System.Drawing.Point(7, 103);
             this.userlastnamelabel.Name = "userlastnamelabel";
-            this.userlastnamelabel.Size = new System.Drawing.Size(61, 13);
+            this.userlastnamelabel.Size = new System.Drawing.Size(68, 14);
             this.userlastnamelabel.TabIndex = 4;
             this.userlastnamelabel.Text = "Last Name:";
             // 
             // addborrowerfirstnametext
             // 
-            this.addborrowerfirstnametext.Location = new System.Drawing.Point(100, 53);
+            this.addborrowerfirstnametext.Location = new System.Drawing.Point(106, 67);
             this.addborrowerfirstnametext.Name = "addborrowerfirstnametext";
-            this.addborrowerfirstnametext.Size = new System.Drawing.Size(117, 20);
+            this.addborrowerfirstnametext.Size = new System.Drawing.Size(128, 22);
             this.addborrowerfirstnametext.TabIndex = 3;
             // 
             // userfirstnamelabel
             // 
             this.userfirstnamelabel.AutoSize = true;
-            this.userfirstnamelabel.Location = new System.Drawing.Point(7, 60);
+            this.userfirstnamelabel.Location = new System.Drawing.Point(7, 74);
             this.userfirstnamelabel.Name = "userfirstnamelabel";
-            this.userfirstnamelabel.Size = new System.Drawing.Size(60, 13);
+            this.userfirstnamelabel.Size = new System.Drawing.Size(69, 14);
             this.userfirstnamelabel.TabIndex = 2;
             this.userfirstnamelabel.Text = "First Name:";
             // 
             // userID
             // 
             this.userID.AutoSize = true;
-            this.userID.Location = new System.Drawing.Point(7, 28);
+            this.userID.Location = new System.Drawing.Point(7, 42);
             this.userID.Name = "userID";
-            this.userID.Size = new System.Drawing.Size(58, 13);
+            this.userID.Size = new System.Drawing.Size(64, 14);
             this.userID.TabIndex = 0;
             this.userID.Text = "Username:";
             // 
@@ -180,9 +191,11 @@
             this.groupBox1.Controls.Add(this.borrowusernametext);
             this.groupBox1.Controls.Add(this.userfirstnametextlabel);
             this.groupBox1.Controls.Add(this.usernamelbl);
-            this.groupBox1.Location = new System.Drawing.Point(570, 74);
+            this.groupBox1.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F);
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(35)))), ((int)(((byte)(43)))));
+            this.groupBox1.Location = new System.Drawing.Point(656, 117);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(223, 135);
+            this.groupBox1.Size = new System.Drawing.Size(253, 135);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             // 
@@ -190,7 +203,7 @@
             // 
             this.usersearchlastname.Location = new System.Drawing.Point(79, 72);
             this.usersearchlastname.Name = "usersearchlastname";
-            this.usersearchlastname.Size = new System.Drawing.Size(137, 20);
+            this.usersearchlastname.Size = new System.Drawing.Size(154, 22);
             this.usersearchlastname.TabIndex = 6;
             // 
             // userlastnametextlabel
@@ -198,15 +211,17 @@
             this.userlastnametextlabel.AutoSize = true;
             this.userlastnametextlabel.Location = new System.Drawing.Point(7, 75);
             this.userlastnametextlabel.Name = "userlastnametextlabel";
-            this.userlastnametextlabel.Size = new System.Drawing.Size(59, 13);
+            this.userlastnametextlabel.Size = new System.Drawing.Size(66, 14);
             this.userlastnametextlabel.TabIndex = 5;
             this.userlastnametextlabel.Text = "Last name:";
             // 
             // searchbookbutton
             // 
-            this.searchbookbutton.Location = new System.Drawing.Point(147, 97);
+            this.searchbookbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.searchbookbutton.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F);
+            this.searchbookbutton.Location = new System.Drawing.Point(147, 99);
             this.searchbookbutton.Name = "searchbookbutton";
-            this.searchbookbutton.Size = new System.Drawing.Size(70, 24);
+            this.searchbookbutton.Size = new System.Drawing.Size(87, 24);
             this.searchbookbutton.TabIndex = 4;
             this.searchbookbutton.Text = "Search";
             this.searchbookbutton.UseVisualStyleBackColor = true;
@@ -216,14 +231,14 @@
             // 
             this.usersearchfirstname.Location = new System.Drawing.Point(80, 44);
             this.usersearchfirstname.Name = "usersearchfirstname";
-            this.usersearchfirstname.Size = new System.Drawing.Size(137, 20);
+            this.usersearchfirstname.Size = new System.Drawing.Size(154, 22);
             this.usersearchfirstname.TabIndex = 3;
             // 
             // borrowusernametext
             // 
             this.borrowusernametext.Location = new System.Drawing.Point(80, 14);
             this.borrowusernametext.Name = "borrowusernametext";
-            this.borrowusernametext.Size = new System.Drawing.Size(137, 20);
+            this.borrowusernametext.Size = new System.Drawing.Size(154, 22);
             this.borrowusernametext.TabIndex = 2;
             // 
             // userfirstnametextlabel
@@ -231,7 +246,7 @@
             this.userfirstnametextlabel.AutoSize = true;
             this.userfirstnametextlabel.Location = new System.Drawing.Point(8, 47);
             this.userfirstnametextlabel.Name = "userfirstnametextlabel";
-            this.userfirstnametextlabel.Size = new System.Drawing.Size(58, 13);
+            this.userfirstnametextlabel.Size = new System.Drawing.Size(67, 14);
             this.userfirstnametextlabel.TabIndex = 1;
             this.userfirstnametextlabel.Text = "First name:";
             // 
@@ -240,7 +255,7 @@
             this.usernamelbl.AutoSize = true;
             this.usernamelbl.Location = new System.Drawing.Point(7, 18);
             this.usernamelbl.Name = "usernamelbl";
-            this.usernamelbl.Size = new System.Drawing.Size(58, 13);
+            this.usernamelbl.Size = new System.Drawing.Size(64, 14);
             this.usernamelbl.TabIndex = 0;
             this.usernamelbl.Text = "Username:";
             // 
@@ -251,7 +266,7 @@
             this.reportToolStripMenuItem});
             this.menuStrip2.Location = new System.Drawing.Point(0, 0);
             this.menuStrip2.Name = "menuStrip2";
-            this.menuStrip2.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip2.Size = new System.Drawing.Size(921, 24);
             this.menuStrip2.TabIndex = 11;
             this.menuStrip2.Text = "menuStrip2";
             // 
@@ -285,17 +300,29 @@
             this.reportToolStripMenuItem.Text = "Report";
             this.reportToolStripMenuItem.Click += new System.EventHandler(this.reportToolStripMenuItem_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Login.Properties.Resources.user_creds_design;
+            this.pictureBox1.Location = new System.Drawing.Point(664, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(77, 73);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
             // borrower
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(921, 561);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip2);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.displayallborrowerbutton);
             this.Controls.Add(this.borrowergridview);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "borrower";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Borrower";
@@ -306,6 +333,7 @@
             this.groupBox1.PerformLayout();
             this.menuStrip2.ResumeLayout(false);
             this.menuStrip2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -338,5 +366,6 @@
         private System.Windows.Forms.TextBox usersearchlastname;
         private System.Windows.Forms.Label userlastnametextlabel;
         private System.Windows.Forms.ComboBox usernamecombobox;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
